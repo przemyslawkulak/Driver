8a0e96ae2f042291aa2d5349d48d0f355ff2eab3
przemyslawkulak <przemyslaw.kulak86@gmail.com>
2019-02-25 18:14:31 +0100

    create app / first migration

--- main/models.py
+++ main/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 # Create your models here.
+
+class Training(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Advice(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class MyUser(AbstractUser):
+    pass
+
+class Message(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Test(models.Model):
+    name = models.CharField(max_length=255)
