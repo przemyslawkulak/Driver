27391ffbdefe9e7f5a1bf6eeaac451cffc2ae560
przemyslawkulak <przemyslaw.kulak86@gmail.com>
2019-02-27 12:22:36 +0100

    models update

--- driver/settings.py
+++ driver/settings.py
@@ -108,6 +108,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 
 try:
     from driver.local_settings import DATABASES
--- main/models.py
+++ main/models.py
@@ -1,27 +1,54 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
 # Create your models here.
-
-class Training(models.Model):
-    name = models.CharField(max_length=255)
-
-
-class Advice(models.Model):
-    name = models.CharField(max_length=255)
+ANSWERS = (
+    (1, 'A'),
+    (2, 'B'),
+    (3, 'C')
+)
 
 
 class Tag(models.Model):
     name = models.CharField(max_length=255)
 
 
+class Advice(models.Model):
+    title = models.CharField(max_length=255)
+    lead = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    img = models.ImageField(blank=True, null=True)
+    film = models.FileField(upload_to='videos/', null=True)
+    description = models.TextField()
+    score = models.IntegerField()
+    tag = models.ManyToManyField(Tag)
+
+
+class Training(models.Model):
+    name = models.CharField(max_length=255)
+    advice = models.ForeignKey(Advice, on_delete=models.CASCADE, )
+    tag = models.ManyToManyField(Tag)
+    total_score = models.IntegerField()
+
+
+class Question(models.Model):
+    question = models.CharField(max_length=255)
+    training = models.ForeignKey(Training, on_delete=models.CASCADE, )
+    answer1 = models.TextField()
+    answer1file = models.FileField(upload_to='videos/', null=True)
+    answer2 = models.TextField()
+    answer2file = models.FileField(upload_to='videos/', null=True)
+    answer3 = models.TextField()
+    answer3file = models.FileField(upload_to='videos/', null=True)
+    correct_answer = models.IntegerField(choices=ANSWERS)
+    tag = models.ManyToManyField(Tag)
+    score = models.IntegerField()
+
+
 class MyUser(AbstractUser):
-    pass
+    user_score = models.IntegerField()
+
 
 class Message(models.Model):
     name = models.CharField(max_length=255)
-
-
-class Test(models.Model):
-    name = models.CharField(max_length=255)
+    user = models.ForeignKey(MyUser, on_delete=models.CASCADE)
